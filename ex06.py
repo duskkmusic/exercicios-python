74ca23c594c9472b9943065bb112de66eabe2794
duskkmusic <contato.duskkoficial@gmail.com>
2025-08-29 17:10:08 -0300

    Exercicio 6 - Vogais por Palavra

---
+++ ex06.py
@@ -0,0 +1,6 @@
+palavras = ("aprender", "programar", "linguagem", "python", "computador")
+for p in palavras:
+    print(f"\nNa palavra {p.upper()} temos as vogais: ", end="")
+    for letra in p:
+        if letra.lower() in "aeiou":
+            print(letra, end=" ")
